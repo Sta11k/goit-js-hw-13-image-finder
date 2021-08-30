9b1e81c5877e24369f48603bad7a9064696a0780
Sta11k <Alexandiel1984@gmail.com>
2021-08-30 14:57:17 +0300

    start

--- src/api/apiService.js
+++ src/api/apiService.js
@@ -1,2 +1,18 @@
 // key:: 23145424-17de0e2191faefedd106abc58
 // https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=что_искать&page=номер_страницы&per_page=12&key=твой_ключ
+export default function apiServise() {
+    const search = "dog";
+    const numberPage = "1";
+fetch(`https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${search}&page=${numberPage}&per_page=12&key=23145424-17de0e2191faefedd106abc58`)
+      .then(response => {
+       
+        return response.json();
+      })
+        .then(data => {
+         maxCantry(data)
+        })
+   
+      .catch(error => {
+         console.log(error,"Немає данних для відправки"); 
+      });
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,46 @@
 import './sass/main.scss';
+import '@pnotify/core/dist/BrightTheme.css';
+import apiServise from "./api/apiService";
+
+import { alert, defaultModules } from '@pnotify/core';
+import * as PNotifyMobile from '@pnotify/mobile';
+
+// defaultModules.set(PNotifyMobile, {});
+
+const refs = {
+   form:document.querySelector("#search-form"),
+  inputSearch: document.querySelector(".form__input"),
+  printGalerry: document.querySelector(".gallery"),
+  submitBtn:document.querySelector(".form__button--submit"),
+  resetBtn: document.querySelector(".form__button--reset"),
+};
+
+alert({
+  text: 'Атата'
+});
+
+
+// const searBtnchImg = e => {
+  
+// }
+
+const hendlerSubmin = (e) => {
+  e.preventDefault()
+  apiServise()
+  // const value = refs.inputSearch.value
+
+  //   fetch(`https://restcountries.eu/rest/v2/name/${value}`)
+  //     .then(response => {
+       
+  //       return response.json();
+  //     })
+  //       .then(data => {
+  //        maxCantry(data)
+  //       })
+   
+  //     .catch(error => {
+  //        console.log(error,"Немає данних для відправки"); 
+  //     });
+}
+
+refs.form.addEventListener('submit', hendlerSubmin);
